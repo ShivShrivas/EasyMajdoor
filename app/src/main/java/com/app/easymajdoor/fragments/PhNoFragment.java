be36add0b0ac984b58ce6bd5bd90eebcec6c2b37
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-10-02 11:33:49 +0530

    feat(auth): Add Ph enter UI

--- app/src/main/java/com/app/easymajdoor/fragments/PhNoFragment.java
+++ app/src/main/java/com/app/easymajdoor/fragments/PhNoFragment.java
@@ -15,6 +15,9 @@ public class PhNoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        
+
         return inflater.inflate(R.layout.fragment_ph_no, container, false);
     }
 }
